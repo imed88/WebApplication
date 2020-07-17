a2f2ff54dca5f7b5c93855888dcc402f99719c52
benzarti <benzimededdine@hotmail.com>
2020-07-17 12:48:23 +0200

    modifications

---
+++ Aplication/Models/Tables/IdentityAppContext.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplication.Models
+{
+    public class IdentityAppContext : DbContext
+    {
+        public IdentityAppContext(DbContextOptions<IdentityAppContext> options) : base(options)
+        {
+        }
+
+      
+
+    }
+}
